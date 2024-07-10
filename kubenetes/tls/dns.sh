320318456c81d699ef538bd8a91110c4d1264daf
kyle-park-io <andy3638@naver.com>
2024-07-10 17:21:23 +0900

    chore: clean up code

--- kubenetes/tls/dns.sh
+++ kubenetes/tls/dns.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 yes Y |
-    sudo certbot delete --cert-name jungho.dev
+  sudo certbot delete --cert-name jungho.dev
 
 sudo ./certbot_venv/bin/certbot certonly \
-    --preferred-challenges dns-01 \
-    -d jungho.dev \
-    --authenticator dns-godaddy \
-    --dns-godaddy-credentials ./godaddy.ini \
-    --dns-godaddy-propagation-seconds 10
+  --preferred-challenges dns-01 \
+  -d jungho.dev \
+  --authenticator dns-godaddy \
+  --dns-godaddy-credentials ./godaddy.ini \
+  --dns-godaddy-propagation-seconds 10
 
 # sudo ./certbot_venv/bin/certbot certonly \
 #     --authenticator dns-godaddy \
