fa9850a47242695026baacf4371d4ab225d0afbd
kyle-park-io <andy3638@naver.com>
2024-08-29 13:08:33 +0900

    refactor: rename ingress-proxy to ingress-reverse-proxy
    - Updated naming from ingress-proxy to ingress-reverse-proxy for clarity
    - Modified all related files and configurations

--- kubenetes/ingress-reverse-proxy/ingress-reverse-proxy-app-k8s/run-server.sh
+++ kubenetes/ingress-reverse-proxy/ingress-reverse-proxy-app-k8s/run-server.sh
@@ -6,5 +6,5 @@ SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 # kubectl apply -f ${SCRIPT_DIR}/../pv/pv.yaml
 
 # server
-kubectl delete deployment ingress-proxy-app-server-deployment
-kubectl apply -f ${SCRIPT_DIR}/ingress-proxy-app-server.yaml
+kubectl delete deployment ingress-reverse-proxy-app-server-deployment
+kubectl apply -f ${SCRIPT_DIR}/ingress-reverse-proxy-app-server.yaml
