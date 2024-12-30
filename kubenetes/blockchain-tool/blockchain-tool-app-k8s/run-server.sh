a1988223c81a633b17c7b77418df2a0aad679d7b
kyle-park-io <andy3638@naver.com>
2024-12-30 20:32:57 +0900

    feat: add Kubernetes configurations for block timestamp collection and token tracking servers - include deployment and service YAML files - configure environment variables for both servers - add resource limits and scaling configurations

--- kubenetes/blockchain-tool/blockchain-tool-app-k8s/run-server.sh
+++
@@ -1,6 +0,0 @@
-SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
-
-# server
-kubectl delete service blockchain-tool-app-server-service
-kubectl delete deployment blockchain-tool-app-server-deployment
-kubectl apply -f ${SCRIPT_DIR}/blockchain-tool-app-server.yaml
---
+++ kubenetes/blockchain-tools/blockchain-tools-app-k8s/run-server.sh
@@ -0,0 +1,10 @@
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+
+# # pvc
+# kubectl delete pvc timestamp-pvc
+# kubectl apply -f ${SCRIPT_DIR}/pvc.yaml
+
+# server
+kubectl delete service blockchain-tools-app-server-service
+kubectl delete deployment blockchain-tools-app-server-deployment
+kubectl apply -f ${SCRIPT_DIR}/blockchain-tools-app-server.yaml
