c0b234a348b64b73ddb7b92f970ad30cfa1a7d00
kyle-park-io <andy3638@naver.com>
2025-01-16 14:46:22 +0900

    feat: add PVC to dex pod

--- kubenetes/dex/dex-app-k8s/run-server.sh
+++ kubenetes/dex/dex-app-k8s/run-server.sh
@@ -1,5 +1,9 @@
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 
+# # pvc
+# kubectl delete pvc dex-pvc
+# kubectl apply -f ${SCRIPT_DIR}/pvc.yaml
+
 # server
 kubectl delete service dex-app-server-service
 kubectl delete deployment dex-app-server-deployment
